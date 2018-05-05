1c9692a74af1b1efa2a8ab3fbc2bd3837fcd5014
Elaine Anderson <eparie@bu.edu>
2018-05-05 14:52:58 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -5,9 +5,9 @@ def fizzbuzz (int)
     "Fizz"
   elsif int % 5 == 0
     "Buzz"
-  elsif int % 15 == 0
+  elsif int % 3 == 0 && int % 5 == 0 
     "FizzBuzz"
-  else int % 4 == 0
+  else 
  nil
   end
 end
